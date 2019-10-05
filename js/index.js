c19e9c4ecdd59c320de0cf5d2bca69aa2394939a
airman66 <53268089+airman66@users.noreply.github.com>
2019-10-05 22:54:13 +0300

    Переписали js скрипт на ванильный javascript, начали работать с eslint, исправили блок intro

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
-$(function () {
-    let gallery = $(".works__content").html();
-    $(".works__more__btn").click(function () {
-        $(".works__content").append(gallery);
-    });
-}); 
+// <vars>
+let galleryBigRow = document.querySelector(".works__content__bigRow").cloneNode();
+galleryBigRow.innerHTML = document.querySelector(".works__content__bigRow").innerHTML;
+let gallerySmallRow = document.querySelector(".works__content__smallRow").cloneNode();
+gallerySmallRow.innerHTML = document.querySelector(".works__content__smallRow").innerHTML;
+// </vars>
+
+// <app>
+// <Gallery>
+document.querySelector(".works__more__btn").addEventListener("click", function () {
+	document.querySelector(".works__content").appendChild(galleryBigRow);
+	galleryBigRow = document.querySelector(".works__content__bigRow").cloneNode();
+	galleryBigRow.innerHTML = document.querySelector(".works__content__bigRow").innerHTML;
+	document.querySelector(".works__content").appendChild(gallerySmallRow);
+	gallerySmallRow = document.querySelector(".works__content__smallRow").cloneNode();
+	gallerySmallRow.innerHTML = document.querySelector(".works__content__smallRow").innerHTML;
+});
+// </Gallery>
+// </app>
